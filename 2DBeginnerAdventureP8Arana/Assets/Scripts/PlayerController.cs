6bd029fef27b1ee6ea2f1b1ab80de51c56f50943
luiss1234 <756101@student.dusd.net>
2024-10-30 15:23:56 -0700

    Added colliders to objects
    I added colliders to my player and objects.

--- 2DBeginnerAdventureP8Arana/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP8Arana/Assets/Scripts/PlayerController.cs
@@ -4,24 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public int maxHealth = 5;
+    int currentHealth;
+
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        float horizontal = Input.GetAxis("Horizontal");
-        float verticle = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
-        Vector2 position = transform.position;
-        position.x = position.x + 8.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 8.0f * verticle * Time.deltaTime; ;
-
-        transform.position = position;
-        
     }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 8.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 8.0f * vertical * Time.deltaTime; ;
+
+        rigidbody2d.MovePosition(position);
+    }
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+
+
+
 }
